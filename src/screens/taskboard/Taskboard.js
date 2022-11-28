1c2976d6aac712ad488279f55dc6807ce2995d0c
ClownSk <cristian97oyarzo@gmail.com>
2022-11-28 17:21:31 -0300

    se añadio los archivos Taskboard.js y task-style

--- src/screens/taskboard/Taskboard.js
+++ src/screens/taskboard/Taskboard.js
@@ -4,7 +4,7 @@ import './task-style.css'
 function Taskboard({ data, label = "description" }){
   return (
     <div className='card1'>
-        <h1> task_board </h1>
+        <h1> Task_board </h1>
         <div className="container d-flex justify-content-center align-items-center h-100">
             <div className='grid-container'>
             {
