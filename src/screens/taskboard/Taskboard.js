1185eba064672d380bd624cde003b2a1cc9e4d1e
ClownSk <cristian97oyarzo@gmail.com>
2022-11-28 17:09:59 -0300

    Se crearon los archivos Taskboard.js y task-style.css

---
+++ src/screens/taskboard/Taskboard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './task-style.css'
+
+function Taskboard({ data, label = "description" }){
+  return (
+    <div className='card1'>
+        <h1> task_board </h1>
+        <div className="container d-flex justify-content-center align-items-center h-100">
+            <div className='grid-container'>
+            {
+                data.map(function (item, index) {
+                    return (
+                        <div className='item1' key={index}>
+                            <h1> Tarea {index+1}</h1>
+                            <p className='card2'>{item[label]}</p>
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </div>
+
+    </div>
+    
+  )
+}
+
+export default Taskboard;
