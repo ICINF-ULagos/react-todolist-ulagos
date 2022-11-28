cbaad4eeb0f9b1e6c0a88ac9bcf7738caa1cf790
[CesarSobarzoHuenchuan] <[cesarignacio.sobarzo@alumnos.ulagos.cl]>
2022-11-28 17:33:41 -0300

    Tarea2

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-
+import TaskBoard from './taskboard/taskBoard'
 import { GenericList } from '../../components/common'
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzcyYWQ3OTAxNWRiMjAwMTc1NGEwZDkiLCJpYXQiOjE2Njg0NjQ0MjV9.YDKiwb-kpBYZ7FqOsJFWwaf35DVZO597NfBfB4YstbI"
@@ -67,7 +67,7 @@ function Home() {
                 <button type="submit">Nueva tarea</button>
             </form>
             
-            <GenericList data={tasks}/>
+            <TaskBoard data={tasks} />
 
             <div style={{ display: 'flex' }}>
 
