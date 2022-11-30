5a8ce86590de3ab5f92fc8ab28277cbb54f4ba99
Nicolas <nicolas.sepulveda@kimenda.cl>
2022-11-30 19:45:32 -0300

    imp: Mejora grafica de formulario de registro.

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 function Login() {
     const [message, setMessage] = useState(null);
     const navigate = useNavigate();
@@ -36,11 +42,33 @@ function Login() {
     }
 
     return (<div>
-        <form onSubmit={handlerSubmit}>
-            <input type="email" name="email" placeholder="Email" />
-            <input type="password" name="password" placeholder="Password" />
-            <button>Enviar</button>
-        </form>
+        <Container>
+            <Row>
+                <Col>
+                    <Form onSubmit={handlerSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" name="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Enviar
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+
 
         {message && <p>{message}</p>}
     </div>)
