aedb6f400e962381420cf835f8c26ae8ccba8dcd
francizxo <francizxo@gmail.com>
2022-11-30 18:19:03 -0300

    2

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -32,7 +32,7 @@ const useUser = (token) => {
         return response.CurrentUSer;
     }
 
-    const removeUser = async (id) => {
+    const removeUser = async (CurrentUSer) => {
         const request = await fetch(`${baseUrl}/task/${id}`, { method: 'DELETE', headers });
         const response = request.ok ? await request.json() : await request.text();
         console.log(response);
@@ -43,16 +43,25 @@ const useUser = (token) => {
         const response = request.ok ? await request.json() : await request.text();
         console.log(response);
     }
-    const loginUSer = async (id, CurrentUSer) => {
-        const request = await fetch("https://api-nodejs-todolist.herokuapp.com/user/login", { method: 'POST', headers, body: JSON.stringify(CurrentUSer) });
+    const loginUSer = async(CurrentUSer)=>{
+
+
+        const request = await fetch(`${baseUrl}/task/${id}`, { method: "POST", body: JSON.stringify(CurrentUSer), headers })
+        const response = request.ok ? await response.json() : await response.text();
+        if(response.ok){
+            sessionStorage.setItem("user", JSON.stringify(response.user));
+            sessionStorage.setItem("token", response.token);
+        }
+        
+        return response.data;
+
+    } 
+    const logoutUSer = async(CurrentUSer)=>{
+        const request = await fetch(`${baseUrl}/task/${id}`, { method: 'POST', headers, body: JSON.stringify(CurrentUSer) });
         const response = request.ok ? await request.json() : await request.text();
-        console.log(response);
-    }
-    const logoutUSer = async (id, CurrentUSer) => {
-        const request = await fetch(`${baseUrl}/task/${id}`, { method: 'GET', headers, body: JSON.stringify(CurrentUSer) });
-        const response = request.ok ? await request.json() : await request.text();
-        console.log(response);
-    }
+        console.log(response)
+    } 
+
     return {
         CurrentUSer,
         setCurrentUser,
