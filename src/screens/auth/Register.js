a3d3c7e035e14c4d5b5bb7524f6289a52385afff
v1nte <ivinte99@gmail.com>
2022-11-30 17:30:43 -0300

    add: Register Screen

--- src/screens/auth/Register.js
+++ src/screens/auth/Register.js
@@ -1,7 +1,51 @@
+import { useRef } from 'react'
+import useUser from '../../hooks/useUser';
+
+
 function Register() {
-    return (<div>
-        <h1>Register</h1>
-    </div>)
+  const { create, isLoading } = useUser()
+
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passRef = useRef()
+  const ageRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const testing = create({
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passRef.current.value,
+      age: ageRef.current.value
+    })
+
+    console.log(testing)
+  }
+
+  return (
+    <div style={{ padding: "5rem 5rem", width: "20rem"}}>
+      <h1> Register </h1>
+      <form style={{
+        display: "flex",
+        flexDirection: "column",
+        }} onSubmit={ handleSubmit }>
+        <input ref={nameRef} placeholder="John Doe"/>
+        <input ref={emailRef} type="email" placeholder="john@doe.con"/>
+        <input ref={passRef} type="password" placeholder="password"/>
+        <input ref={ageRef} type="number" min="12" placeholder="69"/>
+        <button style={{
+          width: "10rem",
+          backgroundColor: "#4CAF50",
+          border: "none",
+          color: "white",
+          textDecoration: "none",
+          textAlign: "center"
+        }}> Registrarse! </button>
+      </form>
+        { isLoading && <p>Some Spinner</p>}
+    </div>
+  )
 }
 
 
